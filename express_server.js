f3621e8b634e2b5bdf86754b0db56d394132b9be
TinyMito <k.ip@me.com>
2023-05-02 00:25:57 -0400

    Remove test codes, update / redirect

--- express_server.js
+++ express_server.js
@@ -40,25 +40,12 @@ const users = {
 /* Initial Setup for Testing
  */
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls/");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-/* app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-}); */
 
 /* Pass variables to EJS template
  */
